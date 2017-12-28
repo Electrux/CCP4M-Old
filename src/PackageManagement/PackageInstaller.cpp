aa5232d503d621a8c4f99b31be7b7e9ea76d8af6
Electrux <ckhandelwal13@gmail.com>
2017-12-29 01:00:13 +0530

    Many changes, bug fix part 1.4

--- src/PackageManagement/PackageInstaller.cpp
+++ src/PackageManagement/PackageInstaller.cpp
@@ -155,8 +155,6 @@ std::map< std::string, std::vector< DirFile > > GetCopyList( const Package & pkg
 		{ "fw", {} }
 	};
 
-	std::cout << "here1" << std::endl;
-
 	if( LocExists( incdir ) ) {
 		GetWildCardFilesInDir( incdir, list[ "inc" ], "*" );
 
@@ -175,8 +173,6 @@ std::map< std::string, std::vector< DirFile > > GetCopyList( const Package & pkg
 			std::cout << BLUE << lst.first << " : " << CYAN << item.dir + item.file << RESET << std::endl;
 		}
 	}
-
-	std::cout << "here2" << std::endl;
 
 	return list;
 }
