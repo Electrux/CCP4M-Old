3fd63ac866c8496501ba59229307b76d8c6ed54a
Electrux <ckhandelwal13@gmail.com>
2017-12-29 17:40:41 +0530

    New output bug test 3

--- src/PackageManagement/PackageBuilder.cpp
+++ src/PackageManagement/PackageBuilder.cpp
@@ -91,7 +91,7 @@ bool BuildDirectory( const Package & pkg )
 		std::cout << YELLOW << "Installing using make install ... " << RESET;
 		std::cout.flush();
 
-		res = DispExecute( install, errors );
+		res = DispExecute( install, errors, false );
 
 		if( res != 0 ) {
 			std::cout << RED << CROSS << std::endl;
