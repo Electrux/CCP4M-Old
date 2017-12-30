fb282046296d3acd3d14809522478955a1ac33d6
Electrux <ckhandelwal13@gmail.com>
2017-12-30 14:29:33 +0530

    Finishing touches to colored outputs part 3.

--- src/ProjectManagement/BuildFilesGenerator.cpp
+++ src/ProjectManagement/BuildFilesGenerator.cpp
@@ -86,7 +86,7 @@ int GenerateBuildFiles()
 			compilestr += " src/" + mainsrc;
 
 			DispColoredData( "[100%]\t", RESET, false );
-			DispColoredData( "Building and Linking" + langstr + "executable:", "build/" + data.name, " ... ",
+			DispColoredData( "Building and Linking " + langstr + " executable:", "build/" + data.name, " ... ",
 					BOLD_YELLOW, BOLD_GREEN, RESET, false );
 
 			int res = ExecuteCommand( compilestr );
