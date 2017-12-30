e85c78648a236f5ca423cd03dc311b9b5706afff
Electrux <ckhandelwal13@gmail.com>
2017-12-31 02:36:17 +0530

    Tar decompressing options and building using cmake, both work!

--- src/PackageManager.cpp
+++ src/PackageManager.cpp
@@ -99,7 +99,7 @@ int PackageManager::InstallPackage( std::string package, bool forceinstall )
 {
 	Package pkg;
 
-	DispColoredData( "Starting package", package, "installation...\n", BOLD_BLUE, BOLD_MAGENTA, BOLD_BLUE, true );
+	DispColoredData( "Starting package", package, "installation ...\n", BOLD_BLUE, BOLD_MAGENTA, BOLD_BLUE, true );
 
 	DispColoredData( "Checking package exists ... " );
 	if( !PackageExists( package, pkg ) ) {
