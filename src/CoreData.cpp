4f6a88c0b2c30930c8ab9b78fb573d9a0c954e83
Electrux <ckhandelwal13@gmail.com>
2018-01-01 12:46:51 +0530

    Extra directory fix didnt work... testing now part 16

--- src/CoreData.cpp
+++ src/CoreData.cpp
@@ -6,7 +6,7 @@
 #include "../include/Paths.hpp"
 
 const std::string GIT_CLONE_CMD = GIT_CMD + " clone https://github.com/Electrux/CCP4M-PKGI.git " + PACKAGE_LIST_DIR;
-const std::string GIT_PULL_CMD = GIT_CMD + " pull " + PACKAGE_LIST_DIR;
+const std::string GIT_PULL_CMD = GIT_CMD + " pull";
 
 Architecture ARCH =
 #ifdef __linux__
